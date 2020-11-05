e98635a69e4f331729decb88ab3be654cba2a09c
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-11-05 07:25:34 -0800

    change _app hrefs

--- pages/_app.js
+++ pages/_app.js
@@ -20,31 +20,31 @@ const MyApp = ({ Component, pageProps }) => {
       <div className="container-page">
         <div className="toolbar-main">
           <div className="header-company">
-            <Link href="http://52.8.24.75:3000">
+            <Link href="/">
               <a className="link trademark">United Medi-Care Inc.</a>
             </Link>
           </div>
           <div className="container-links">
-            <Link href="http://52.8.24.75:3000">
+            <Link href="/">
               <a className="link">About</a>
             </Link>
-            <Link href='http://52.8.24.75:3000/products'>
+            <Link href='/products'>
               <a className="link">Products</a>
             </Link>
-            <Link href="http://52.8.24.75:3000/contact">
+            <Link href="/contact">
               <a className="link">Contact</a>
             </Link>
           </div>
           <img onClick={() => toolbarHandler(false)} className="hamburger" src="/hamburger_button_white.svg" alt="hamburger button" />
         </div>
         <div className={showToolbar ? "container-links-small-device" : "container-links-small-device hidden"}>
-          <Link href="http://52.8.24.75:3000">
+          <Link href="/">
             <a onClick={() => toolbarHandler(false)} className="link dropdown">About</a>
           </Link>
-          <Link href="http://52.8.24.75:3000/products">
+          <Link href="/products">
             <a onClick={() => toolbarHandler(true)} className="link dropdown">Products</a>
           </Link>
-          <Link href="http://52.8.24.75:3000/contact">
+          <Link href="/contact">
             <a onClick={() => toolbarHandler(false)} className="link dropdown">Contact</a>
           </Link>
         </div>
