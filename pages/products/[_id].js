b3e272b554fdc1fc14759091df52f0980eb0aa40
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-11-17 00:07:13 -0800

    add getstaticprops to product details page

--- pages/products/[_id].js
+++ pages/products/[_id].js
@@ -1,29 +1,30 @@
 import Router, { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import Axios from 'axios';
+// import Axios from 'axios';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import model from '../../database/model.js';
 
-const ProductDetails = () => {
+const ProductDetails = (props) => {
 
-  const [product, setProduct] = useState(null);
+  // const [product, setProduct] = useState(null);
   const [index, setIndex] = useState(0);
   const [showModal, setShowModal] = useState(false);
   const [animation, setAnimation] = useState('modal-hidden');
-  const router = useRouter();
-  const { _id } = router.query;
+  // const router = useRouter();
+  // const { _id } = router.query;
 
-  useEffect(() => {
-    if (_id) {
-      Axios
-        .get(`/api/products/${_id}`)
-        .then(response => {
-          setProduct(response.data);
-        })
-        .catch(err => console.error(err));
-    }
-  }, [_id])
+  // useEffect(() => {
+  //   if (_id) {
+  //     Axios
+  //       .get(`/api/products/${_id}`)
+  //       .then(response => {
+  //         setProduct(response.data);
+  //       })
+  //       .catch(err => console.error(err));
+  //   }
+  // }, [_id])
 
   const modalHandler = (e) => {
     if (showModal) {
@@ -59,21 +60,21 @@ const ProductDetails = () => {
   return (
     <div className="page-admin">
       <div onClick={modalHandler} className={animation === "active" ? "modal active" : `modal ${animation}`} >
-        <img onClick={modalHandler} className={animation === "active" ? "modal-image active" : `modal-image ${animation}`} src={product && product.images.length ? product.images[index].fireBaseUrl : '/placeholder-image.png'} alt="modal_image" />
+        <img onClick={modalHandler} className={animation === "active" ? "modal-image active" : `modal-image ${animation}`} src={props.product && props.product.images.length ? props.product.images[index].fireBaseUrl : '/placeholder-image.png'} alt="modal_image" />
         <div className={animation === "active" ? "modal-background" : `modal-background ${animation}`}></div>
       </div>
       <h2>Product Details</h2>
       <div className="row-about">
         <div className="column">
           <div className="container-image-about-admin" style={{ "alignSelf": "flexStart", "margin": "0 0 20px 0" }}>
-            <img onClick={modalHandler} className="image-product" src={product && product.images.length ? product.images[index].fireBaseUrl : '/placeholder-image.png'} alt="product-view" />
+            <img onClick={modalHandler} className="image-product" src={props.product && props.product.images.length ? props.product.images[index].fireBaseUrl : '/placeholder-image.png'} alt="product-view" />
           </div>
           <Slider {...settings} className="slider">
             {
-              product ?
-                product.images.map((image, index) => {
+              props.product ?
+                props.product.images.map((image, key) => {
                   return (
-                    <div>
+                    <div key={key}>
                       <img data-index={index} onClick={selectPhoto} style={{ "height": "100%", "width": "100%", "cursor": "pointer" }} src={image.fireBaseUrl} alt="product_thumb" />
                     </div>
                   )
@@ -83,27 +84,27 @@ const ProductDetails = () => {
         </div>
         <div className="column" style={{ "maxWidth": "90vw" }}>
           <p className="paragraph-about">
-            <b>{product ? product.name : null}</b>
+            <b>{props.product ? props.product.name : null}</b>
           </p>
           <p className="paragraph-about">
-            <b>Product ID: </b>{product ? product.prodId : null}
+            <b>Product ID: </b>{props.product ? props.product.prodId : null}
           </p>
           <p className="paragraph-about" style={{ "display": "flex", "alignItems": "flexStart" }}>
             <b>Price: </b>
             <button className="button-see-products" onClick={toContact}>Get quotes</button>
           </p>
           <p className="paragraph-about">
-            {product ? product.description : null}
+            {props.product ? props.product.description : null}
           </p>
           <p className="paragraph-about" style={{ "marginBottom": "0" }}>
             <b>Features: </b>
           </p>
           <ul className="paragraph-about">
             {
-              product ?
-                product.features.map(feature => {
+              props.product ?
+                props.product.features.map((feature, key) => {
                   return (
-                    <li style={{ "width": "80%" }}>
+                    <li key={key} style={{ "width": "80%" }}>
                       {feature}
                     </li>
                   )
@@ -115,10 +116,10 @@ const ProductDetails = () => {
           </p>
           <ul className="paragraph-about">
             {
-              product ?
-                product.specs.map(spec => {
+              props.product ?
+                props.product.specs.map((spec, key) => {
                   return (
-                    <li style={{ "width": "80%" }}>
+                    <li key={key} style={{ "width": "80%" }}>
                       {spec}
                     </li>
                   )
@@ -126,17 +127,17 @@ const ProductDetails = () => {
             }
           </ul>
           {
-            product && product.downloads.length ?
+            props.product && props.product.downloads.length ?
               <p className="paragraph-about" style={{ "marginBottom": "0" }}>
                 <b>Downloads: </b>
               </p> : null
           }
           <ul className="paragraph-about">
             {
-              product ?
-                product.downloads.map(download => {
+              props.product ?
+                props.product.downloads.map((download, key) => {
                   return (
-                    <li style={{ "width": "80%" }}>
+                    <li key={key} style={{ "width": "80%" }}>
                       <a href={download.fireBaseUrl} target="_blank">{download.title}</a>
                     </li>
                   )
@@ -150,3 +151,14 @@ const ProductDetails = () => {
 }
 
 export default ProductDetails;
+
+export async function getStaticProps({ params }) {
+  let response = await model.getOneProduct(params._id);
+
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(response))
+    },
+    revalidate: 10
+  }
+}
